9533a09d097ffce4d454b8925a077e684a519ba9
Alex13090 <kohninnaung@gmail.com>
2021-12-17 11:04:13 +0100

    add list hours (2)

--- meteo.js
+++ meteo.js
@@ -71,14 +71,6 @@ function requestApi(event) {
                 display.appendChild(icon);
                 display.appendChild(pCond);
                 display.appendChild(pTempMax);
-                for(let i = 0; i > 23; i++) {
-                    let hours = day.hourly_data[i];
-                    let iconSmall = document.createElement("img");
-                    iconSmall.src = hours.ICON;
-                    let p = document.createElement("p");
-                    p.textContent = `$[i]H00 ${iconSmall.src} ${hours.CONDITION}`;
-                    display.appendChild(p);
-                }
             }
             // for forecast of hour
             // for(let i = 0; i > 23; i++) {
